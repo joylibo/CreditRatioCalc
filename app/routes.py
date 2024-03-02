3cf4bc8f89d25e64812a1fa015d638a237a73470
李勃 <258696131@qq.com>
2024-03-02 23:10:49 +0800

    添加similarity方法
    是一种考虑了欧式距离的方法

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify
 from transformers import BertTokenizer, BertModel
+from scipy.spatial.distance import euclidean
 import torch
 from app import app
 
@@ -60,3 +61,38 @@ def cosine_sim_bert():
     else:
         result = 0
     return jsonify({'result': result})
+
+@app.route('/similarity', methods=['POST'])
+def similarity():
+    data = request.json
+    text1 = data.get('text1', '')
+    text2 = data.get('text2', '')
+
+    if text1 and text2:
+        # 使用分词器处理文本
+        encoded_input1 = tokenizer(text1, return_tensors='pt')
+        encoded_input2 = tokenizer(text2, return_tensors='pt')
+
+        # 使用模型生成文本嵌入
+        with torch.no_grad():
+            model_output1 = model(**encoded_input1)
+            model_output2 = model(**encoded_input2)
+
+        # 获取最后一层隐藏状态的第一个token（CLS token）作为句子的表示
+        sentence_embedding1 = model_output1.last_hidden_state[:, 0, :]
+        sentence_embedding2 = model_output2.last_hidden_state[:, 0, :]
+    
+        # 计算两个句子嵌入的余弦相似度
+        cosine_similarity = torch.nn.functional.cosine_similarity(sentence_embedding1, sentence_embedding2)
+        
+        # 计算两个句子嵌入的欧氏距离
+        euclidean_distance = euclidean(sentence_embedding1.squeeze().numpy(), sentence_embedding2.squeeze().numpy())
+        
+        # 综合考虑余弦相似度和欧氏距离，这里使用简单的平均值
+        similarity_score = (cosine_similarity.item() + 1 / (1 + euclidean_distance)) / 2
+        
+        result = similarity_score
+    else:
+        result = 0
+    return jsonify({'result': result})
+

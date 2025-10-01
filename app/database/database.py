832d982c84dd492e1d0fa114c05a30ab3a4dc9f8
李勃 <258696131@qq.com>
2025-10-01 11:44:08 +0800

    更新数据库连接方式

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,12 @@
 from sqlmodel import create_engine
 from app.config.credit_db_conf import DATABASE_CONFIG as DB_CONF
 
-# 创建数据库连接
-database_url = f"mysql+pymysql://{DB_CONF['username']}:{DB_CONF['password']}@{DB_CONF['hostname']}:{DB_CONF['port']}/{DB_CONF['database']}"
-engine = create_engine(database_url)
+# 使用连接参数而不是URL字符串
+database_url = f"mysql+pymysql://{DB_CONF['hostname']}:{DB_CONF['port']}/{DB_CONF['database']}"
+engine = create_engine(
+    database_url,
+    connect_args={
+        'user': DB_CONF['username'],
+        'password': DB_CONF['password']
+    }
+)
